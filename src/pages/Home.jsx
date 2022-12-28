56ebf990e25d91b95ff0dfbdf0ca8e8b38edc271
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-28 19:02:49 +0000

    Remove projects from main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import WorkExperience from '../components/WorkExperience';
 import Available from '../components/Available';
-import MyWork from '../components/MyWork';
 // import LatestBlog from '../components/LatestBlog';
 import ContactMe from '../components/ContactMe';
 import Footer from '../components/Footer';
@@ -15,7 +14,6 @@ const Home = () => (
     <Nav />
     <Hero />
     <About />
-    <MyWork />
 
     <Available />
     <WorkExperience />
