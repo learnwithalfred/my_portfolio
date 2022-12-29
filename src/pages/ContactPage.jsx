56326063e8ca2b169689e39d4074f509d82aab02
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-29 04:12:56 +0000

    Update contact form UI

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -20,15 +20,15 @@ const ContactPage = () => (
                 <h3 className="mb-6 text-2xl leading-normal font-medium">
                   Let's talk
                 </h3>
-             
+
                 <div className="flex items-center mt-6">
                   <div className>
                     <h5 className="title font-bold mb-0">Email</h5>
                     <a
-                      href="mailto:contact@example.com"
+                      href="mailto:learnwithalfred@gmail.com"
                       className="btn btn-link text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out"
                     >
-                      contact@example.com
+                      learnwithalfred@gmail.com
                     </a>
                   </div>
                 </div>
@@ -36,34 +36,33 @@ const ContactPage = () => (
                   <div className>
                     <h5 className="title font-bold mb-0">Phone</h5>
                     <a
-                      href="tel:+152534-468-854"
+                      href="tel:+233541034798"
                       className="btn btn-link text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out"
                     >
-                      +152 534-468-854
+                      +233541034798
                     </a>
                   </div>
                 </div>
                 <div className="flex items-center mt-6">
                   <div className>
-                    <h5 className="title font-bold mb-0">Location</h5>
+                    <h5 className="title font-bold mb-0">LinkedIn</h5>
                     <a
-                      href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+                      href="https://www.linkedin.com/in/learnwithalfred/"
                       data-type="iframe"
                       className="video-play-icon btn btn-link text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 transition duration-500 lightbox"
                     >
-                      View on Google map
+                      linkedin.com/in/learnwithalfred
                     </a>
                   </div>
                 </div>
-              
+
                 {/*end icon*/}
               </div>
-         
             </div>
           </div>
           <div className="lg:col-span-8 md:col-span-6 mt-8 md:mt-0">
             <div className="lg:ml-5">
-              <div className="bg-white dark:bg-slate-900 rounded-md shadow dark:shadow-gray-800 p-6">
+              <div className="bg-white dark:bg-slate-900 rounded-md  dark:shadow-gray-800 p-6">
                 <h3 className="mb-6 text-2xl leading-normal font-medium">
                   Get in touch !
                 </h3>
