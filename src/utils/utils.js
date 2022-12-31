6faca5a4fc302c23fda37008dd116e7828ef8d33
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-31 17:37:35 +0000

    Add hamet to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { toast } from "react-toastify";
 
 export const API_KEY = "";
@@ -13,3 +14,24 @@ export const handleToast = ({ msg, type = 'success' }) =>
     theme: 'colored',
     type,
   });
+
+// handle Helmet
+
+export const handleHelmet = ({ title, description, image }) => {
+  return (
+    <Helmet >
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
+      <meta property="og:url" content={window} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Helmet>
+  );
+};
