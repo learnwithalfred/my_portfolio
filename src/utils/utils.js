2435516cf211e6fbc3c01fde0f96807acfc3646a
Alfred Boateng <programmingwithalfred@gmail.com>
2023-01-02 06:01:35 +0000

    Update title rendering function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,21 +17,18 @@ export const handleToast = ({ msg, type = 'success' }) =>
 
 // handle Helmet
 
-export const RenderTitle = ({ title, description, image }) => {
-  return (
-    <Helmet >
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="image" content={image} />
-      <meta property="og:url" content={window} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </Helmet>
-  );
-};
+export const RenderTitle = ({
+  title = 'Alfred Boateng',
+  description = 'A mid level software developer',
+}) => (
+  <Helmet>
+    <title>{title} </title>
+    <meta property="og:url" content={window} />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+  </Helmet>
+);
