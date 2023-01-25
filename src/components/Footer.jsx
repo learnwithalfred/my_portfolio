9ca6b412e28f0f0c7ce5e09ac3004116f7fea551
Alfred Boateng <programmingwithalfred@gmail.com>
2023-01-25 21:58:22 +0000

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { SocialIcons } from './SocialIcons';
+import { Github, Linkedin, Twitter } from './Icons';
+
 const Footer = () => {
   return (
-    <footer className="footer bg-dark-footer relative text-gray-200 dark:text-gray-200">
+    <footer className="footer relative bg-slate-900/70 text-gray-800 dark:text-gray-200">
       <div className="py-[30px] px-0 border-t border-slate-800">
         <div className="container text-center">
           <div className="grid md:grid-cols-3 grid-cols-1 items-center">
             <div className="md:text-left text-center">
-              <Link to="/" className="text-[22px] focus:outline-none">
+              <Link to="/" className=" focus:outline-none">
                 Alfred Boateng
-                </Link>
+              </Link>
             </div>
             <div className="text-center mt-6 md:mt-0"></div>
-            <ul className="list-none md:text-right text-center mt-6 md:mt-0">
-              <li className="inline">
-                <a
-                  href="https://linkedin.com/in/learnwithalfred"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"
-                >
-                  <i className="uil uil-linkedin" title="Linkedin" />
-                </a>
-              </li>
-              <li className="inline">
-                <a
-                  href="https://github.com/learnwithalfred"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"
-                >
-                  <i className="uil uil-github" title="Linkedin" />
-                </a>
-              </li>
-              <li className="inline">
-                <a
-                  href="https://twitter.com/kb_alfred"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"
-                >
-                  <i className="uil uil-twitter align-middle" title="twitter" />
-                </a>
-              </li>
-              <li className="inline">
-                <a
-                  href="mailto:learnwithalfred@gmail.com"
-                  className="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"
-                >
-                  <i className="uil uil-envelope align-middle" title="email" />
-                </a>
-              </li>
-            </ul>
+            <SocialIcons
+              profiles={[
+                {
+                  name: 'Github',
+                  url: 'https://www.github.com/learnwithalfred',
+                  icon: Github,
+                },
+                {
+                  name: 'LinkedIn',
+                  url: 'https://www.linkedin.com/in/learnwithalfred',
+                  icon: Linkedin,
+                },
+                {
+                  name: 'Twitter',
+                  url: 'https://twitter.com/Kb_alfred',
+                  icon: Twitter,
+                },
+              ]}
+            />
           </div>
         </div>
       </div>
