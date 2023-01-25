f3506108be7fd00c5159e17143e3bdb34430babe
Alfred Boateng <programmingwithalfred@gmail.com>
2023-01-25 21:58:08 +0000

    Create a social icons component

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,26 @@
+import { Icon } from './Icons';
+
+const SocialIcons = ({ profiles }) => {
+  return (
+    <ul className="list-none md:text-right text-center mt-6 md:mt-0">
+      {profiles.map(({ url, name, icon }) => (
+        <li
+          className="w-6 h-6 opacity-70 hover:opacity-100 transition inline"
+          key={name}
+        >
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            title={name}
+            className="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"
+          >
+            <img src={icon} alt={name} className="w-6 h-6" />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export { SocialIcons };
